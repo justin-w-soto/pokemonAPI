c0af4dcaec7a8a241f744e0a9727b3505c2b2e4c
Justin W Soto <soto@pdx.edu>
2021-08-06 12:51:01 -0700

    updated search params based on Julies morning example

--- src/App.js
+++ src/App.js
@@ -3,53 +3,80 @@ import PokeList from './PokeList.js';
 import './App.css';
 
 class App extends Component {
-  state = { data: [], loading: true, query: null };
+  state = { data: [], loading: true, query: null, sortOrder: 'asc' };
 
   componentDidMount() {
       this.fetchData();
   }
 
   fetchData = async () => {
-      let url = 'https://pokedex-alchemy.herokuapp.com/api/pokedex';
-      if (this.state.query) {
-          url = url + `?pokemon=${this.state.query}`;
-      }
+    if (!this.state.loading) {
+      this.setState({loading: true });
+    }
 
+    let url = 'https://pokedex-alchemy.herokuapp.com/api/pokedex';
+    let searchParams = new URLSearchParams();
+
+    if (this.state.query) {
+      searchParams.set('pokemon', this.state.query);
+  }
+
+    if (this.state.sortOrder) {
+    searchParams.set('sort', 'pokemon');
+    searchParams.set('direction', this.state.sortOrder);
+}
+
+      url = url + `?${searchParams.toString()}`;
       let response = await fetch(url);
       let data = await response.json();
-      this.setState({ data: data.results });
+      this.setState({ data: data.results, loading: false });
   };
 
   updateQuery = (e) => {
       this.setState({ query: e.target.value });
   };
 
+  updateSort = (e) => {
+    this.setState({ sortOrder: e.target.value });
+};
 
   render() {
-    if(!this.state.loading) {
-
-      return <h1>ERROR!</h1>
-    }
-
-    else{
-      return (
+    const { loading, sortOrder } = this.state;
+    return(
           <div>
 
-              <h1> PoKeDeX API 😛 </h1>
-                <h3> Please be patient with me, I am slow.</h3>
-              
-                  <section className="body">
-                      <input onChange={this.updateQuery} type="text"></input>
-                      <br></br>
-                      <button onClick={this.fetchData}>Gotta Catch 'em All!</button>
+              <h1> PoKeDeX PaRtY 😛 </h1> 
+
+              <select defaultValue={sortOrder} onChange={this.updateSort}>
+
+                    <option value="asc">Ascending</option>
+                    <option value="desc">Descending</option>
+
+              </select>
+
+                <input onChange={this.updateQuery} type="text"></input>
+
+                <button onClick={this.fetchData}>Search!</button>
+
+                {loading && <h3> 🐌 PLEASE BE PATIENT, I MOVE SLOW 🐌 </h3>}
+
+                {!loading && (
+
+                    <section>
+
                       <PokeList pokemon={this.state.data} />
+
                   </section>
-            
+                    
+
+                    
+                )}
 
           </div>
       );
-  }}
+  }
 }
+
 export default App;
 
 
