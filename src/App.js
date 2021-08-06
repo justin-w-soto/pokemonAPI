d3c0806ae2af3cd7408d7e33900778d4782b0c20
Justin W Soto <soto@pdx.edu>
2021-08-05 17:16:37 -0700

    initial state is set with list of pokemon. need to get search btn to work now

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+import PokeList from './PokeList.js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  state = { data: [], loading: true, query: null };
 
+  componentDidMount() {
+      this.fetchData();
+  }
+
+  fetchData = async () => {
+      let url = 'https://pokedex-alchemy.herokuapp.com/api/pokedex';
+      if (this.state.query) {
+          url = url + `?pokemon=${this.state.query}`;
+      }
+
+      let response = await fetch(url);
+      let data = await response.json();
+      this.setState({ data: data.results });
+  };
+
+  updateQuery = (e) => {
+      this.setState({ query: e.target.value });
+  };
+
+
+  render() {
+    if(!this.state.loading) {
+      
+      return <h1>ERROR!</h1>
+    }
+
+    else{
+      return (
+          <div>
+
+              <h1> PoKeDeX API 😛 </h1>
+                <h3> Please be patient with me, I am slow.</h3>
+              
+                  <section>
+                      <input onChange={this.updateQuery} type="text"></input>
+                      <button onClick={this.fetchData}>Gotta Catch 'em All!</button>
+                      <PokeList pokemon={this.state.data} />
+                  </section>
+            
+
+          </div>
+      );
+  }}
+}
 export default App;
+
+
---
+++ src/PokeItem.js
@@ -0,0 +1,17 @@
+// Displays a pokemon image, name, and other fun data (attack, defence, type).
+import React, { Component } from 'react';
+
+class PokeItem extends Component {
+    render() {
+        return (
+            <ul>
+
+                <h2>{ this.props.data.pokemon }</h2>
+                <img src={ this.props.data.url_image } alt={"pokemon-pics"}/>
+
+            </ul>
+        );
+    }
+}
+
+export default PokeItem;
